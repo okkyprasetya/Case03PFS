844ff313e1013c4b43cafb3cd4eaa522ae450b77
okkyprasetya19 <okkyprasetya19@gmail.com>
2024-11-07 18:25:46 +0700

    Adding publish files on publish folder

--- obj/Debug/net8.0/SalesOrder.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesOrder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaebdbafe579fb2516cc3c4fa3a2799b11b9672f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1625dbfc2c583e0df2538d01992e88761f5abf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
